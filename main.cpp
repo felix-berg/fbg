8aa8a9687e54b73b1e61f4961902af3e67a74522
felix-berg <felixbpedersen@gmail.com>
2022-03-19 15:59:31 +0100

    the

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include "2D_Graphics_Lib.hpp"
 
 int main() {
-	LoopWin win;
-	win.run();
-	return 0;
+	// Construct 1280x720 Window object.
+	LoopWin window { "Window Title", 1280, 720 };
+
+	// set window attributes:
+	window.background(0, 20);
+	window.framerate(144);
+	
+	// Create circle object at (0, 0) with radius 50
+	Circle circle {0, 0, 50};
+	
+	// draw circle object at every frame update,
+	// by attaching it to the window object
+	window.attach(circle);
+
+	// define function that will be called at every frame
+	window.before_draw = [&](float frametime) -> void {
+		// grab the mouse position from the window
+		V2d<int> mpos = window.get_mouse_pos();
+
+		// set the position of the radius to the
+		// current mouse position
+		circle.pos(mpos);
+	};
+
+	// Start the game loop by calling run.
+	window.run(); 
+
+	return 0; // :)
 }
