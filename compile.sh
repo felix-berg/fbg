b4e719b1573123613ccc489e3a2f424f2383dd94
felix-berg <felixbpedersen@gmail.com>
2022-04-07 19:43:58 +0200

    removed -H

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ echo "Lines of code: " $(wc -l *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cp
 "
 
 echo "Compiling..."
-g++-11 -std=c++20  *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cpp Graphics_Lib/Shapes/drawing_algorithms/*.cpp Graphics_Lib/Shapes/BMP/*.cpp -lSDL2 -mavx2 -pthread -O3 -H
+g++-11 -std=c++20  *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cpp Graphics_Lib/Shapes/drawing_algorithms/*.cpp Graphics_Lib/Shapes/BMP/*.cpp -lSDL2 -mavx2 -pthread -O3
 
 echo "Finished."
---
+++ rm_precompiled.sh
@@ -0,0 +1,5 @@
+rm ./Graphics_Lib/*.hpp.gch 
+rm ./Graphics_Lib/Shapes/*.hpp.gch 
+rm ./Graphics_Lib/Shapes/drawing_algorithms/*.hpp.gch 
+rm ./Graphics_Lib/Shapes/BMP/*.hpp.gch 
+rm 2D_Graphics_Lib.hpp.gch 
