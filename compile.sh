030003626c7f3157c9301e0444ce2b261ace219e
felix-berg <felixbpedersen@gmail.com>
2022-04-07 09:46:24 +0200

    Allow moving of contexts

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ rm a.out
 echo "Lines of code: " $(wc -l *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cpp Graphics_Lib/*.hpp Graphics_Lib/Shapes/*.hpp Graphics_Lib/Shapes/drawing_algorithms/*.cpp Graphics_Lib/Shapes/drawing_algorithms/*.hpp Graphics_Lib/Shapes/BMP/*.cpp Graphics_Lib/Shapes/BMP/*.hpp | tr -d ' ' | awk 'END {print $NF}') "
 "
 
-g++ -std=c++20 *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cpp Graphics_Lib/Shapes/drawing_algorithms/*.cpp Graphics_Lib/Shapes/BMP/*.cpp -lSDL2 -mavx2 -pthread -O3
+echo "Compiling..."
+g++-11 -std=c++20  *.cpp Graphics_Lib/*.cpp Graphics_Lib/Shapes/*.cpp Graphics_Lib/Shapes/drawing_algorithms/*.cpp Graphics_Lib/Shapes/BMP/*.cpp -lSDL2 -mavx2 -pthread -O3
 
-echo "Finished.
-"
+echo "Finished."
